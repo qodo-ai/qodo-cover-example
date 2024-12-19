fd2224e23bf73f69544c49f2ef3a937a42e8804b
David Wurtz <david.w@qodo.ai>
2024-12-19 12:21:05 -0800

    add php toy app

---
+++ templated_tests/php/myapp/tests/CalculatorTest.php
@@ -0,0 +1,30 @@
+<?php
+use MyApp\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase {
+    public function testAdd() {
+        $calc = new Calculator();
+        $this->assertEquals(4, $calc->add(2, 2));
+    }
+
+    public function testMultiply() {
+        $calc = new Calculator();
+        $this->assertEquals(6, $calc->multiply(2, 3));
+    }
+
+    public function testSubtract() {
+        $calc = new Calculator();
+        $this->assertEquals(1, $calc->subtract(3, 2));
+    }
+
+    public function testSquareRoot() {
+        $calc = new Calculator();
+        $this->assertEquals(3, $calc->squareRoot(9));
+    }
+
+    public function testPower() {
+        $calc = new Calculator();
+        $this->assertEquals(8, $calc->power(2, 3));
+    }
+}
