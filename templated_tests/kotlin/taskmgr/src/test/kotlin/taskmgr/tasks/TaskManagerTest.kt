87af3bafd0734c978ff9ce525fb45166a32b43d0
Qodo Cover <cover-bot@qodo.ai>
2025-01-15 00:24:19 +0000

    add tests

--- templated_tests/kotlin/taskmgr/src/test/kotlin/taskmgr/tasks/TaskManagerTest.kt
+++ templated_tests/kotlin/taskmgr/src/test/kotlin/taskmgr/tasks/TaskManagerTest.kt
@@ -30,4 +30,59 @@ class TaskManagerTest {
         assertFalse(tasks[0].done)
     }
 
+    @Test
+    fun `clear all tasks`() {
+        val task1 = Task("Task 1")
+        val task2 = Task("Task 2")
+        taskManager.add(task1)
+        taskManager.add(task2)
+        
+        taskManager.clearAll()
+        
+        val tasks = taskManager.list()
+        assertTrue(tasks.isEmpty())
+    }
+
+
+    @Test
+    fun `update task title by index`() {
+        val task = Task("Old Title")
+        taskManager.add(task)
+        
+        taskManager.update(0, "New Title")
+        
+        val tasks = taskManager.list()
+        assertEquals("New Title", tasks[0].title)
+    }
+
+
+    @Test
+    fun `remove task by index`() {
+        val task1 = Task("Task 1")
+        val task2 = Task("Task 2")
+        taskManager.add(task1)
+        taskManager.add(task2)
+        
+        taskManager.remove(0)
+        
+        val tasks = taskManager.list()
+        assertEquals(1, tasks.size)
+        assertEquals("Task 2", tasks[0].title)
+    }
+
+
+    @Test
+    fun `mark task as done by index`() {
+        val task1 = Task("Task 1")
+        val task2 = Task("Task 2")
+        taskManager.add(task1)
+        taskManager.add(task2)
+        
+        taskManager.markDone(1)
+        
+        val tasks = taskManager.list()
+        assertTrue(tasks[1].done)
+    }
+
+
 }
